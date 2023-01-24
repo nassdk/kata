44c18433359c19fc69c0f88a4e9f589fbf07f648
Sadyk Nasrullaev <nassdk17@yandex-team.ru>
2023-01-24 22:05:36 +0300

    -> Kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.8.0"
 }
-
-group = "me.ruanss5"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -19,6 +16,6 @@ tasks.test {
     useJUnit()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
