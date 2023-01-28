74e4bfc36fceadc7f4c15227ce8de146e791d073
Sadyk Nasrullaev <nassdk17@yandex-team.ru>
2023-01-29 02:46:07 +0300

    Kata update.

--- src/main/kotlin/kata/Going to the cinema.kt
+++ src/main/kotlin/kata/Going to the cinema.kt
@@ -1,17 +1,23 @@
 package kata
 
 import kotlin.math.ceil
+import kotlin.math.pow
 
 fun main() {
-    print(movie(100, 10, 0.95))
+    print(movie(card = 500, ticket = 15, perc = 0.9))
 }
 
 fun movie(card: Int, ticket: Int, perc: Double): Int {
-    var number = perc * ticket
-    var count = 0
-    while (ceil(number + card) >= ticket * count) {
-        number *= perc
+    var count = 0.0
+    var sum = 0.0
+    while (ticket * count <= ceil(sum) + card) {
+        println("current count $count")
+        println("current perc pow ${perc.pow(count)}")
+        println("current sum ${ceil(sum) + card}")
+        println("-------------")
+
         count++
+        sum += ticket * perc.pow(count)
     }
-    return count
+    return count.toInt()
 }
